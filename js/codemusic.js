effa7f5bc167d762207a80a5f00172797e9a37a5
nbsteuv <nbsteuv@gmail.com>
2017-10-01 15:43:18 -0400

    Added error handling and display

--- js/codemusic.js
+++ js/codemusic.js
@@ -2,6 +2,15 @@ function codemusic(){
 
 	var code = document.getElementById("code").value;
 	console.log(code);
-	eval(code);
 
+	try{
+		eval(code);
+	} catch(e){
+		displayError(e);
+	}
+
+}
+
+function displayError(e){
+	document.getElementById("code-error").innerHTML = e;
 }
