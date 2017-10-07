7c6ee23069eb0fa1f66f90cabadb66d03a64d94f
nbsteuv <nbsteuv@gmail.com>
2017-10-07 12:21:05 -0400

    Added support for chords and note string parsing to allow two-argument play calls

--- js/codemusic.js
+++ js/codemusic.js
@@ -8,6 +8,8 @@ var CodeMusicInterpreter;
 CodeMusicInterpreter = {
 	soundId: 0,
 	playList: [],
+	octaveRange: [1, 8],
+	noteRange: ['A', 'B', 'C', 'D', 'E', 'F', 'G'],
 	runPlayListItems: async function(){
 		for(var i = 0; i < this.playList.length; i++){
 			var playListItem = this.playList[i];
@@ -19,10 +21,54 @@ CodeMusicInterpreter = {
 				case 'rest':
 					await wait(playListItem.seconds);
 					break;
+				case 'chord':
+					for(var noteIndex = 0; noteIndex < playListItem.notes.length; noteIndex++){
+						this.playNote(playListItem.notes[noteIndex]);
+					}
+					await wait(playListItem.seconds);
+					break;
 				default:
 					throw new Error('Play type not implemented');
 			}
 		}
+	},
+	parseNote: function(noteString){
+		//Validate note string length
+		if(noteString.length > 3){
+			throw new Error('The note cannot be more than 3 characters long and must include a note and octave');
+		}
+		if(noteString.length < 2){
+			throw new Error('The note cannot be less than 2 characters long and must include a note and octave');
+		}
+
+		//Validate octave character
+		var octave = noteString[noteString.length - 1];
+		if(isNaN(octave)){
+			throw new Error('Octave (the last character in the note) must be a number 1-8');
+		}
+		var octaveRange = this.octaveRange;
+		if(octave < octaveRange[0]){
+			octave = octaveRange[0];
+		}
+		if(octave > octaveRange[1]){
+			octave = octaveRange[1];
+		}
+
+		//Validate note character and optional sharp
+		var note;
+		if(this.noteRange.indexOf(noteString[0].toUpperCase()) === -1){
+			throw new Error('Note must be a letter A-G');
+		}
+		if(noteString.length === 3){
+			if(noteString[1] !== '#'){
+				throw new Error('Note must be a letter A-G plus an optional sharp symbol (#)');
+			}
+			var note = noteString.slice(0,2).toUpperCase();
+		} else {
+			var note = noteString[0].toUpperCase();
+		}
+
+		return {note: note, octave: octave};
 	},
 	buildNote: function(note, octave, seconds){
 		var note = {
@@ -42,6 +88,14 @@ CodeMusicInterpreter = {
 			, seconds: seconds
 		}
 		return rest;
+	},
+	buildChord: function(noteArray, seconds){
+		var chord = {
+			type: 'chord',
+			notes: noteArray,
+			seconds: seconds
+		};
+		return chord;
 	},
 	addToPlayList: function(object){
 		this.playList.push(object);
@@ -75,14 +129,36 @@ async function codemusic(){
 
 }
 
-function play(note, octave, seconds){
-	var note = CodeMusicInterpreter.buildNote(note, octave, seconds);
+function play(noteString, seconds){
+	var parsedNote = CodeMusicInterpreter.parseNote(noteString);
+	var note = CodeMusicInterpreter.buildNote(parsedNote.note, parsedNote.octave, seconds);
 	CodeMusicInterpreter.addToPlayList(note);
 }
 
 function rest(seconds){
 	var rest = CodeMusicInterpreter.buildRest(seconds)
 	CodeMusicInterpreter.addToPlayList(rest);
+}
+
+function playChord(){
+	var seconds = arguments[arguments.length - 1];
+	if(typeof(seconds) !== 'number'){
+		throw new Error('Duration of chord must be a number.');
+	}
+	var parsedNoteArray = [];
+	for(var i = 0; i < arguments.length - 1; i++){
+		try{
+			var noteString = arguments[i];
+			var parsedNote = CodeMusicInterpreter.parseNote(noteString);
+			parsedNoteArray.push(parsedNote);
+		} catch(e){
+			var noteNumber = i + 1;
+			var errorMessage = 'Problem in note ' + noteNumber + ' in chord -- ' + e;
+			throw new Error(errorMessage);
+		}
+	}
+	var chord = CodeMusicInterpreter.buildChord(parsedNoteArray, seconds);
+	CodeMusicInterpreter.addToPlayList(chord);
 }
 
 function clearPlayList(){
