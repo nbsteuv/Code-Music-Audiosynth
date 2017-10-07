d271c80c2df9f2d89611605cb149abec9382ddbd
nbsteuv <nbsteuv@gmail.com>
2017-10-07 18:15:41 -0400

    Added audiosync script and wired up CodeMusicService

--- CodeMusic/src/app/app.component.ts
+++ CodeMusic/src/app/app.component.ts
@@ -1,22 +1,32 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Provider} from '@angular/core';
+import {CodeMusicService} from './services/codemusic.service';
+
+declare var Synth: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [CodeMusicService]
 })
 export class AppComponent {
 
   code: string;
+  displayError: string;
+
+  constructor(private CodeMusicInterpreter: CodeMusicService){}
 
   run(){
+    this.CodeMusicInterpreter.testMethod();
     try{
       eval(this.code);
       //CodeMusicInterpreter.runPlayListItems();
     } catch(e){
-      //displayError(e);
-      console.log(e);
+      this.displayError = e;
     }
   }
+
+
 
 }
---
+++ CodeMusic/src/app/services/codemusic.service.ts
@@ -0,0 +1,8 @@
+import {Injectable} from '@angular/core';
+
+@Injectable()
+export class CodeMusicService{
+    testMethod(){
+        console.log('testicus test rex');
+    }
+}
